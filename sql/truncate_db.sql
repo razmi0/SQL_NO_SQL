451ac6f32a76fc4e3833f5b4d8f99681dcc03cdd
razmiO <thomas.cuesta31@gmail.com>
2024-10-30 16:05:24 +0100

    Add script truncate_db to empty tables out

---
+++ sql/truncate_db.sql
@@ -0,0 +1,34 @@
+
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE `avis`;
+
+TRUNCATE TABLE `activite`;
+
+TRUNCATE TABLE `emplacement_equipement`;
+
+TRUNCATE TABLE `emplacement_sejour`;
+
+TRUNCATE TABLE `equipement`;
+
+TRUNCATE TABLE `participation`;
+
+TRUNCATE TABLE `sejour`;
+
+TRUNCATE TABLE `commande_code_promo`;
+
+TRUNCATE TABLE `commande`;
+
+TRUNCATE TABLE `emplacement`;
+
+TRUNCATE TABLE `code_promo`;
+
+TRUNCATE TABLE `utilisateur`;
+
+TRUNCATE TABLE `mode_paiement`;
+
+TRUNCATE TABLE `type`;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+
