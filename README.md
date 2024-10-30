c1eb1a72c1fc78f3d0635ad2def4c6fb5b4b4124
razmiO <thomas.cuesta31@gmail.com>
2024-10-30 13:43:52 +0100

    Rework project structure

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # SQL NO SQL
 
-exercice [SQL 1](./camping_bdd.md)
+## Notes
+
+- [init_db](./sql//init_db.sql) initialise les tables de la base de données ( à la mano)
+- [migrate_dummy](./sql/migrate_dummy.sql) peuple la base de données ( à la mano)
+
+## Liens
+
+- Exercice 1 :
+  - [consigne](./exercices/1/consigne.md)
+  - [devoir](./exercices/1/devoir.sql)
+
+## Repo structure
+
+```plaintext
+
+📁 SQL_NO_SQL
+    📁 cours
+    📁 exercices
+        📁 1
+            ─ consigne.md
+            ─ devoir.sql
+        ─ 2
+    📁 sql
+        ─ init_db.sql
+        ─ migrate_dummy.sql
+    ─ .DS_Store
+    ─ .gitignore
+    ─ pdf_conception_camping_TP.pdf
+    ─ README.md
+```
--- exercices/1/devoir.sql
+++ exercices/1/devoir.sql
@@ -1,39 +1,10 @@
-# camping
-
-Reprendre la conception du projet Camping
-
-## Construction de la structure de la bdd
-
-à l'aide de PMA ( php my admin) ou en ligne de commande, construire la structure de la BDD :
-
-- pensez aux clés primaires, étrangères et tables pivot
-- appliquez un maximum de contraintes et de valeurs par défault
-
-## Alimenter votre bdd avec des jeux de données
-
-## Requêtes de type Select
-
-- Récupérer l'ensemble des emplacements disponibles pour une période définie
-- Récupérer l'ensemble des données afin de générer une facture pdf par rapport à un Id Client et l'Id d'une commande
-
-```sql
+-- Récupérer l'ensemble des emplacements disponibles pour une période définie
 
 
--- -- Active: 1728385530506@@127.0.0.1@3306@db_camping
 
-# Inserting dummy data to the database
+-- Récupérer l'ensemble des données afin de générer une facture pdf par rapport à un Id Client et l'Id d'une commande
 
-# DONE
-
-
-# Not done
-
-
-##Requêtes de type Select :
-
--- - Récupérer l'ensemble des emplacements disponibles pour une période définie
--- - Récupérer l'ensemble des données afin de générer une facture pdf par rapport à un Id Client et l'Id d'une commande
--- -
+# Cette requetes est provoque duplication de données car ne peut récupérer qu'une activité par commande (or une commande peut contenir plusieurs activités)
 
 SELECT
 cp.code as promo_code, cp.valeur as promo_valeur, cp.unite as promo_unite,
@@ -41,7 +12,7 @@ mp.nom as paiement,
 s.date_debut as debut_sejour, s.date_fin as sejour_fin,
 s.nombre_adultes as sejour_adults, s.nombre_enfants as sejour_enfants, s.nombre_voitures as sejour_voiture, s.nombre_animaux as sejour_animaux,
 e.caution as emplacement_caution, e.prix_bs as emplacement_prix_BS,e.prix_hs as emplacement_prix_HS,
-p.nombre_adultes as participation_activite_adults  , p.nombre_enfants as participation_activite_enfants,
+p.nombre_adultes as participation_activite_adults , p.nombre_enfants as participation_activite_enfants,
 a.nom as activite_nom,a.prix_adulte_bs as activite_prix_BS,a.prix_adulte_hs as acitivte_prix_adulte_HS,a.prix_enfant_bs as activite_prix_enfant_BS,a.prix_enfant_hs as activite_prix_enfant_HS,a.prix_animal as activite_prix_animal,
 u.nom as client_nom, u.prenom as client_prenom, u.email as client_email
 FROM commande as c
@@ -55,6 +26,3 @@ LEFT JOIN participation as p ON s.id_sejour = p.id_sejour
 LEFT JOIN activite as a ON p.id_activite = a.id_activite
 LEFT JOIN utilisateur as u ON c.id_utilisateur = u.id_utilisateur
 WHERE c.id_commande = 1
-
-
-```
