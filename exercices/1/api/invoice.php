44aa2a41393e73876e2fff0e45b2b8da7f9265f2
razmiO <thomas.cuesta31@gmail.com>
2024-11-01 03:27:14 +0100

    Add facture data get endpoint

---
+++ exercices/1/api/invoice.php
@@ -0,0 +1,26 @@
+<?php
+
+
+require_once "../../../php/dao/Connection.php";
+require_once "../../../php/dao/Procs.php";
+
+use PHP\DAO\Connection;
+use PHP\DAO\Procs;
+
+$id = 1;
+$connection = new Connection("db_camping");
+$pdo = $connection->getPDO();
+$procs = new Procs($pdo);
+
+[$code_promo, $md_paiement, $emplacement, $activite, $client] = $procs->getInvoiceFromCommandId($id);
+
+$data_invoice = [
+    "code_promo" => $code_promo,
+    "md_paiement" => $md_paiement,
+    "emplacement" => $emplacement,
+    "activite" => $activite,
+    "client" => $client
+];
+
+header('Content-Type: application/json');
+echo json_encode($data_invoice, JSON_UNESCAPED_SLASHES);
