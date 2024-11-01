2248a6100898509cffaa8505ffd697e904df4df7
razmiO <thomas.cuesta31@gmail.com>
2024-11-01 03:26:31 +0100

    Add procedures calls

--- exercices/1/procs.sql
+++ exercices/1/procs.sql
@@ -1,6 +1,9 @@
 
--- Récupérer l'ensemble des emplacements disponibles pour une période définie
-DELIMITER //
+--
+--
+-- -- Récupérer l'ensemble des emplacements disponibles pour une période définie
+--
+--
 CREATE PROCEDURE getAvailableEmplacements( IN in_date_debut DATE, IN in_date_fin DATE)
 BEGIN
 SELECT emplacement.id_emplacement
@@ -10,32 +13,36 @@ LEFT JOIN sejour ON emplacement_sejour.id_sejour = sejour.id_sejour
 WHERE (sejour.date_fin < '2024-07-01' OR sejour.date_debut > '2024-07-10')
 OR sejour.date_debut IS NULL;
 END;
-DELIMITER ;
--- Récupérer l'ensemble des données afin de générer une facture pdf par rapport à un Id Client et l'Id d'une commande
+--
+--
+--
+--
+--
 
--- Je vais creer les procédures associé au devoir 1 afin de pouvoir les appelé depuis un serveur PHP
+-- Récupérer elements d'une facture
+--
+--
+--
+--
 
-DELIMITER //
 CREATE PROCEDURE getInvoiceFromCommandId( IN in_command_id INT)
 BEGIN
 
--- 1. le code promo
 
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_code_promo AS
 SELECT code_promo.code as code_promo, code_promo.valeur as code_promo_valeur, code_promo.unite as code_promo_unite
 FROM commande
 LEFT JOIN commande_code_promo ON commande.id_commande = commande_code_promo.id_commande
 LEFT JOIN code_promo ON commande_code_promo.id_code_promo = code_promo.id_code_promo
 WHERE commande.id_commande = in_command_id;
 
--- 2. le mode de paiement
-
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_mode_paiement AS
 SELECT mode_paiement.nom as mode_de_paiement
 FROM commande
 LEFT JOIN mode_paiement ON commande.id_mode_paiement = mode_paiement.id_mode_paiement
 WHERE commande.id_commande = in_command_id;
 
--- 3. les emplacements et leurs types pour le sejour pour la commande
-
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_emplacement AS
 SELECT type.nom as emplacement_type, emplacement.caution as emplacement_caution, emplacement.prix_bs as emplacement_prix_bs, emplacement.prix_hs as emplacement_prix_hs,
 sejour.date_debut as sejour_date_debut, sejour.date_fin as sejour_date_fin, sejour.nombre_adultes as sejour_nombre_adultes, sejour.nombre_enfants as sejour_nombre_enfants, sejour.nombre_voitures as sejour_nombre_voitures, sejour.nombre_animaux as sejour_nombre_animaux
 FROM commande
@@ -45,8 +52,7 @@ LEFT JOIN emplacement ON emplacement_sejour.id_emplacement = emplacement.id_empl
 LEFT JOIN type ON emplacement.id_type = type.id_type
 WHERE commande.id_commande = in_command_id;
 
--- 4. les activités pour le sejour pour la commande
-
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_activite AS
 SELECT activite.nom as activite_nom, activite.prix_adulte_bs as activite_prix_adulte_bs, activite.prix_adulte_hs as activite_prix_adulte_hs, activite.prix_enfant_bs as activite_prix_enfant_bs, activite.prix_enfant_hs as activite_prix_enfant_hs, activite.prix_animal as activite_prix_animal,
 participation.nombre_adultes as participation_nombre_adultes, participation.nombre_enfants as participation_nombre_enfants
 FROM commande
@@ -55,6 +61,20 @@ LEFT JOIN participation ON sejour.id_sejour = participation.id_sejour
 LEFT JOIN activite ON participation.id_activite = activite.id_activite
 WHERE commande.id_commande = in_command_id;
 
-END;
+-- utilisateur
 
-DELIMITER ;
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_utlisateurs AS
+SELECT utilisateur.nom as utilisateur_nom, utilisateur.prenom as utilisateur_prenom, utilisateur.email as utilisateur_email
+FROM commande
+LEFT JOIN utilisateur ON commande.id_utilisateur = utilisateur.id_utilisateur
+WHERE commande.id_commande = in_command_id;
+
+-- convenient selects with temp tables
+
+SELECT * FROM temp_code_promo;
+SELECT * FROM temp_mode_paiement;
+SELECT * FROM temp_emplacement;
+SELECT * FROM temp_activite;
+SELECT * FROM temp_utlisateurs;
+
+END
