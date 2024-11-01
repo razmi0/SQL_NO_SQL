dee23586a5dc9dd50a77bedc01ec8a2e7a0a65cf
razmiO <thomas.cuesta31@gmail.com>
2024-11-01 03:27:41 +0100

    Add Proc class with the queries and the process query

---
+++ php/dao/Procs.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace PHP\DAO;
+
+use Exception;
+use PDO;
+
+/**
+ * 
+ * Class Procs
+ * 
+ * @method static getInvoiceFromCommandId (id INT)
+ * @method static getAvailableEmplacements( date_debut DATETIME date_fin DATETIME)
+ */
+class Procs
+{
+
+    private $pdo = null;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * getInvoiceFromCommandId
+     * @param id INT
+     */
+
+    public function getInvoiceFromCommandId($id)
+    {
+        return $this->runProc("CALL getInvoiceFromCommandId($id);", $this->pdo);
+    }
+
+    /**
+     * getAvailableEmplacements
+     * @param date_debut DATETIME
+     * @param date_fin DATETIME
+     */
+
+    public function getAvailableEmplacements($date_debut, $date_fin)
+    {
+        return $this->runProc("CALL getAvailableEmplacements($date_debut, $date_fin);", $this->pdo);
+    }
+
+    /**
+     * runProc
+     * @param string $proc_call
+     * @param PDO $pdo
+     * @return array
+     */
+    private function runProc(string $proc_call, PDO $pdo)
+    {
+        $result = [];
+        try {
+            $pdo->beginTransaction();
+            $stmt = $pdo->prepare($proc_call);
+            $stmt->execute();
+            do {
+                $result[] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } while ($stmt->nextRowset());
+            $pdo->commit();
+        } catch (Exception $e) {
+            $pdo->rollBack();
+            echo "Failed: " . $e->getMessage();
+        }
+        return $result;
+    }
+}
