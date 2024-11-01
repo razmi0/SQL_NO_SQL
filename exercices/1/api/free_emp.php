8c55c2c008a8046bc2f7656215325c5aa0eb7b1a
razmiO <thomas.cuesta31@gmail.com>
2024-11-01 19:25:16 +0100

    Add spaces

--- exercices/1/api/free_emp.php
+++ exercices/1/api/free_emp.php
@@ -6,22 +6,20 @@ require_once "../../../php/dao/Procs.php";
 require_once "../../../php/http/Request.php";
 require_once "../../../php/http/Response.php";
 
+
 use PHP\DAO\Connection;
 use PHP\DAO\Procs;
 use PHP\HTTP\Request;
 use PHP\HTTP\Response;
 
 
-
 $request = new Request();
 $date_debut = $request->getQueryParam("date_debut");
 $date_fin = $request->getQueryParam("date_fin");
-
 $connection = new Connection("db_camping");
 $pdo = $connection->getPDO();
 $procs = new Procs($pdo);
 
 [$emplacement] = $procs->getAvailableEmplacements($date_debut, $date_fin);
-
 
 Response::send(["data" => $emplacement]);
